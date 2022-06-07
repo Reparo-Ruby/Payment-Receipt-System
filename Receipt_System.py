0377579bb6c58772f22ca8e164840787dd04d0cb
Sinesethu <stukani021@students.wethinkcode.co.za>
2022-06-07 04:56:57 +0200

    Added ability to add stock

--- Receipt_System.py
+++ Receipt_System.py
@@ -2,6 +2,7 @@ import os
 import json
 from re import S
 
+
 def first_time_user():
 
     print("Provide Management Details")
@@ -14,8 +15,6 @@ def first_time_user():
     for_receipt = open("store.txt","w+")
     for_receipt.write(f"{store}")
     for_receipt.close()
-
-    does_employee_work_here()
 
 
 def add_employees():
@@ -30,10 +29,13 @@ def check_user():
     manager_exists = os.path.exists('management.txt')
     employees_exist = os.path.exists("employees.txt")
     
-
     if manager_exists == False and employees_exist == False:
         first_time_user()
         add_employees()
+    
+    elif employees_exist == False:
+        add_employees()
+
     else:
         returning_user()
 
@@ -51,8 +53,10 @@ def does_employee_work_here(employee):
         whats_next = input("What would you like to do next?\n")
         if "stock" in whats_next:
             stock_market()
-        if "payment" in whats_next:
-            receive_payments()    
+        elif "payment" in whats_next:
+            receive_payments()
+        else:
+            print("System does not understand command.\nUse keywords 'payment' or 'stock'.")
     else: 
         with open('store.txt') as store:
             shop = store.readline()
@@ -72,7 +76,7 @@ def log_employees(employee):
         each_employee = open(f"{i+1}. {name}.txt","w+")
 
     print("You have successfully configured the system to work with your store")
-    print("Please rerun the programme to start getting your receipts")
+    print("Please rerun the programme to start adding stock and getting your receipts")
     
 
 def returning_user():
@@ -88,7 +92,7 @@ def receive_payments():
 def take_stock():
    
     what_item = input("what item would you like to see?\n")
-    with open('stock', 'w+') as stock:
+    with open('stock.txt', 'w+') as stock:
         details = stock.read()
     if what_item in details:
         print(f"You have {details['Quantity']} {what_item} left. ")
@@ -96,32 +100,34 @@ def take_stock():
         print(f"{what_item} is not in the stores stock list.")    
 
 
-def add_stock(employee,file):
+def add_stock():
 
-    details = {"Quantity": quantity,
-          "stock": stock,
-          "price": price }
-
-    with open('stock', 'w+') as stock:
-        stock.write(json.dumps(details))
-    pass
-
-    stock = input("Name/Lable of the item\n")
-    quantity = input(f"How many {stock} are you adding?\n")
-
-    if employee in file: ##this is for the manager we will only check if the manager exists only she is allowed to change prices
+    still_adding = input("Still adding?....\n")
+    while still_adding.upper() == "YES":
+        stock = input("Name/Lable of the item\n")
+        quantity = input(f"How many {stock}'s are you adding?\n")
         price = input(f"Price of {stock}\nR ")
+
+        details = {"Quantity": quantity,
+            "stock": stock,
+            "price": price }        
+
+        with open('stock.txt', 'w+') as stock:
+            stock.write(json.dumps(details))
 
 
 def stock_market():
 
-    add_or_take = input("Do you want to do Stock Taking or Add Stock?")
-    if add_or_take.lower() == "stock taking": 
-        take_stock()
+    add_or_take = input("Do you want to do Stock Taking or Add Stock?\n")
+    if add_or_take.lower() == "stock taking":
+        if os.path.exists('store.txt') == True:
+            take_stock()
+        else:
+            print("There is no stock in the store.")    
     elif add_or_take.lower() == "add stock":
         add_stock()    
     
-    file1 = open("convert.txt", "r")
+    file1 = open("store.txt", "r")
     print(file1.read())
 
 
